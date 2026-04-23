296aa090afb38cd176e6635dd57919236e7eb460
Kelly C <kelly.crabbe@gmail.com>
2026-04-23 16:50:15 +0200

    Refactor lottery game to support multiple players

--- lesson1-lottery.py
+++ lesson1-lottery.py
@@ -1,19 +1,54 @@
 # Predefine number
-# User has to guess the number
 # Declare constant value for winning
 WINNER = 8
 
-# Declare input to var
-name = input("Enter your name: ")
-user_input = input("Guess the number: ")
-num_of_guesses = 0
+# Define scoreboard
+leaderboard = {}
+
+
+def play_lottery(player_name):
+    # Handles 1 player
+    # runs the loop and checks for q as exit
+    # output is number of guesses
+    num_of_guesses = 0  # putting this in the function so it resets when func is called
+    while True:
+        user_input = input("Guess the number: ")
+
+        if user_input.lower() in ["q", "quit"]:
+            return None
+
+        if not user_input.isnumeric():
+            print("Invalid input. Please enter a number.")
+            continue
+        elif int(user_input) != WINNER:
+            num_of_guesses += 1
+        else:
+            return num_of_guesses
+
+
+def print_leaderboard(leaderboard):
+    headers = ["Name", "Score"]
+    print("|-------------------|")
+    print(f"| {headers[0]:<10}| {headers[1]:<5}|")
+    print("|-------------------|")
+    for name, score in leaderboard.items():
+        print(f"| {name:<10}| {score:<5}|")
+    print("|-------------------|")
+
 
 while True:
-    print("You guessed it! The number was", WINNER)
-    print("That took", num_of_guesses, "guesses")
+    player_name = input("Enter your name to play: ")
 
-    if user_input == "q" or user_input == "Q":
+    result = play_lottery(player_name)
+
+    if result is None:
+        print(f"{player_name} quit the game.")
+    else:
+        leaderboard[player_name] = result
+        print(f"{player_name} guessed the number in {result} wrong guesses.")
+
+    again = input("Does anyone else want to play? (y/n): ")
+    if again == "n":
         break
-    elif int(user_input) != WINNER:
-        user_input = input("Guess the number: ")
-        num_of_guesses += 1
+
+print_leaderboard(leaderboard)
