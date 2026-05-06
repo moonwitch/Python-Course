cc60a0efd32dba49ca6a214abe191560fbc0f8b5
Kelly C <kelly.crabbe@gmail.com>
2026-05-06 15:08:04 +0200

    Change func to sum the strings (concat lol)

--- params.py
+++ params.py
@@ -1,5 +1,12 @@
+# def function_with_params(var1, var2):
+#     print(var1, var2)
+
+
+# function_with_params("one", "two")
+
+
 def function_with_params(var1, var2):
-    print(var1, var2)
+    print(var1 + var2)
 
 
 function_with_params("one", "two")
